c640c8b002e5917e9e1f5e9c9916b4a63962fec8
Denis Gulko <denis.gulko.1998@gmail.com>
2026-02-17 21:38:08 +0200

    hm7

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,12 @@
 const userName = document.querySelector('#name-input');
 const userNameElem = document.querySelector('#name-output');
 
-userName.addEventListener('input', () => { 
-    const value = userName.value;
+userName.addEventListener('input', () => {
+  const trimmedValue = userName.value.trim();
 
-
-    userNameElem.textContent = value;
-
+  if (trimmedValue === '') {
+    userNameElem.textContent = 'Anonymous';
+  } else {
+    userNameElem.textContent = trimmedValue;
+  }
 });
